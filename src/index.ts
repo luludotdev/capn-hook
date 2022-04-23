d27050073e70806e5cc3c9b2025c94e258d90564
Jack Baron <jackmbaron@gmail.com>
2022-04-23 22:18:50 +0100

    generate schema automatically on commit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import 'source-map-support/register.js'
 
+import mkdirp from 'mkdirp'
+import { writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
 import { argv } from 'node:process'
 import { jsonSchema } from './schema.js'
 
 const main = async () => {
   if (argv[2]?.toLowerCase() === 'schemagen') {
-    const schema = JSON.stringify(jsonSchema, null, 2)
-    console.log(schema)
+    const dir = join('.', 'config')
+    await mkdirp(dir)
+
+    const path = join(dir, 'config.schema.json')
+    const schema = JSON.stringify(jsonSchema, null, 2) + '\n'
+    await writeFile(path, schema)
 
     return
   }
