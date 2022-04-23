24582063f941229c0e4dafae8993a7548bfc0496
Jack Baron <jackmbaron@gmail.com>
2022-04-23 22:07:45 +0100

    setup schema validation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
 import 'source-map-support/register.js'
+
+import { argv } from 'node:process'
+import { jsonSchema } from './schema.js'
+
+const main = async () => {
+  if (argv[2]?.toLowerCase() === 'schemagen') {
+    const schema = JSON.stringify(jsonSchema, null, 2)
+    console.log(schema)
+
+    return
+  }
+
+  // TODO
+  void 0
+}
+
+main().catch(console.error)
---
+++ src/schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod'
+import { zodToJsonSchema } from 'zod-to-json-schema'
+
+export type WebhookEmbed = z.infer<typeof WebhookEmbedSchema>
+export const WebhookEmbedSchema = z.object({
+  title: z.string().nonempty(),
+})
+
+export type Webhook = z.infer<typeof WebhookSchema>
+export const WebhookSchema = z.object({
+  id: z.string().nonempty(),
+  embed: WebhookEmbedSchema,
+})
+
+export const jsonSchema = zodToJsonSchema(WebhookSchema)
