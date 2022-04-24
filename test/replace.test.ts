9bee5114a3764e1853ab758159eef8f139a7db29
Jack Baron <jackmbaron@gmail.com>
2022-04-25 00:00:21 +0100

    fix tests

--- test/replace.test.ts
+++ test/replace.test.ts
@@ -4,45 +4,45 @@ import { replace } from '../src/replace.js'
 // #region
 test('replace(): parses strings', t => {
   const data = { value: 'value' }
-  const resolved = replace('{{ value }}', data)
+  const resolved = replace(data, '{{ value }}')
 
   t.is(resolved, 'value')
 })
 
 test('replace(): parses numbers', t => {
   const data = { value: 2 }
-  const resolved = replace('{{ value }}', data)
+  const resolved = replace(data, '{{ value }}')
 
   t.is(resolved, '2')
 })
 
 test('replace(): parses bigints', t => {
   const data = { value: BigInt(Number.MAX_SAFE_INTEGER) + BigInt(10) }
-  const resolved = replace('{{ value }}', data)
+  const resolved = replace(data, '{{ value }}')
 
   t.is(resolved, '9007199254741001')
 })
 
 test('replace(): throws for arrays', t => {
   const data = { value: [] }
-  t.throws(() => replace('{{ value }}', data))
+  t.throws(() => replace(data, '{{ value }}'))
 })
 
 test('replace(): throws for objects', t => {
   const data = { value: {} }
-  t.throws(() => replace('{{ value }}', data))
+  t.throws(() => replace(data, '{{ value }}'))
 })
 // #endregion
 
 // #region
 // #region
 test('replace(): does not exist, is not lazy, no value -> throws', t => {
-  t.throws(() => replace('{{ a.b }}', {}))
+  t.throws(() => replace({}, '{{ a.b }}'))
 })
 
 test('replace(): does exist, is not lazy, no value -> resolves to value of path', t => {
   const data = { a: { b: 2 } }
-  const resolved = replace('{{ a.b }}', data)
+  const resolved = replace(data, '{{ a.b }}')
 
   t.is(resolved, '2')
 })
@@ -50,13 +50,13 @@ test('replace(): does exist, is not lazy, no value -> resolves to value of path'
 
 // #region
 test('replace(): does not exist, is lazy, no value -> empty string', t => {
-  const resolved = replace('{{ ?a.b }}', {})
+  const resolved = replace({}, '{{ ?a.b }}')
   t.is(resolved, '')
 })
 
 test('replace(): does exist, is lazy, no value -> resolves to value of path', t => {
   const data = { a: { b: 2 } }
-  const resolved = replace('{{ ?a.b }}', data)
+  const resolved = replace(data, '{{ ?a.b }}')
 
   t.is(resolved, '2')
 })
@@ -64,12 +64,12 @@ test('replace(): does exist, is lazy, no value -> resolves to value of path', t 
 
 // #region
 test('replace(): does not exist, is not lazy, has value -> throws', t => {
-  t.throws(() => replace('{{ a.b:"value" }}', {}))
+  t.throws(() => replace({}, '{{ a.b:"value" }}'))
 })
 
 test('replace(): does exist, is not lazy, has value -> resolves to additional value', t => {
   const data = { a: { b: 2 } }
-  const resolved = replace('{{ a.b:"value" }}', data)
+  const resolved = replace(data, '{{ a.b:"value" }}')
 
   t.is(resolved, 'value')
 })
@@ -77,13 +77,13 @@ test('replace(): does exist, is not lazy, has value -> resolves to additional va
 
 // #region
 test('replace(): does not exist, is lazy, has value -> resolves to additional value', t => {
-  const resolved = replace('{{ ?a.b:"value" }}', {})
+  const resolved = replace({}, '{{ ?a.b:"value" }}')
   t.is(resolved, 'value')
 })
 
 test('replace(): does exist, is lazy, has value -> resolves to value of path', t => {
   const data = { a: { b: 2 } }
-  const resolved = replace('{{ ?a.b:"value" }}', data)
+  const resolved = replace(data, '{{ ?a.b:"value" }}')
 
   t.is(resolved, '2')
 })
