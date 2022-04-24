85ba59ae764617b03d87a6ecbcc6258d89b2276e
Jack Baron <jackmbaron@gmail.com>
2022-04-24 18:47:10 +0100

    refactor out embed generation

---
+++ src/embed.ts
@@ -0,0 +1,53 @@
+import { type HexColorString, MessageEmbed } from 'discord.js'
+import { createReplace } from './replace.js'
+import { type Webhook } from './schema.js'
+
+export const generateEmbed: <T extends Record<string, unknown>>(
+  hook: Webhook,
+  data: Readonly<T>
+) => MessageEmbed = (hook, data) => {
+  const replace = createReplace(data)
+  const embed = new MessageEmbed()
+
+  embed.setTitle(replace(hook.embed.title))
+  if (hook.embed.description) {
+    embed.setDescription(replace(hook.embed.description))
+  }
+
+  if (hook.embed.url) embed.setURL(replace(hook.embed.url))
+  if (hook.embed.image) embed.setImage(replace(hook.embed.image))
+  if (hook.embed.thumbnail) {
+    embed.setThumbnail(replace(hook.embed.thumbnail))
+  }
+
+  if (hook.embed.author) {
+    const name = replace(hook.embed.author.name)
+    const url = hook.embed.author.url && replace(hook.embed.author.url)
+    const iconURL =
+      hook.embed.author.iconURL && replace(hook.embed.author.iconURL)
+
+    embed.setAuthor({ name, url, iconURL })
+  }
+
+  if (hook.embed.footer) {
+    const text = replace(hook.embed.footer.text)
+    const iconURL =
+      hook.embed.footer.iconURL && replace(hook.embed.footer.iconURL)
+
+    embed.setFooter({ text, iconURL })
+  }
+
+  if (hook.embed.color) {
+    const color = replace(hook.embed.color)
+    embed.setColor(color as HexColorString)
+  }
+
+  for (const field of hook.embed.fields ?? []) {
+    const name = replace(field.name)
+    const content = replace(field.content)
+
+    embed.addField(name, content, field.inline)
+  }
+
+  return embed
+}
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import Router from '@koa/router'
-import { type HexColorString, MessageEmbed, WebhookClient } from 'discord.js'
+import { WebhookClient } from 'discord.js'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import Koa from 'koa'
 import koaBody from 'koa-body'
-import { createReplace } from './replace.js'
+import { generateEmbed } from './embed.js'
 import { type Config } from './schema.js'
 
 export const createServer = async (config: Config) => {
@@ -36,50 +36,8 @@ export const createServer = async (config: Config) => {
         },
       })
 
-      const replace = createReplace(data)
-      const embed = new MessageEmbed()
-
-      embed.setTitle(replace(hook.embed.title))
-      if (hook.embed.description) {
-        embed.setDescription(replace(hook.embed.description))
-      }
-
-      if (hook.embed.url) embed.setURL(replace(hook.embed.url))
-      if (hook.embed.image) embed.setImage(replace(hook.embed.image))
-      if (hook.embed.thumbnail) {
-        embed.setThumbnail(replace(hook.embed.thumbnail))
-      }
-
-      if (hook.embed.author) {
-        const name = replace(hook.embed.author.name)
-        const url = hook.embed.author.url && replace(hook.embed.author.url)
-        const iconURL =
-          hook.embed.author.iconURL && replace(hook.embed.author.iconURL)
-
-        embed.setAuthor({ name, url, iconURL })
-      }
-
-      if (hook.embed.footer) {
-        const text = replace(hook.embed.footer.text)
-        const iconURL =
-          hook.embed.footer.iconURL && replace(hook.embed.footer.iconURL)
-
-        embed.setFooter({ text, iconURL })
-      }
-
-      if (hook.embed.color) {
-        const color = replace(hook.embed.color)
-        embed.setColor(color as HexColorString)
-      }
-
-      for (const field of hook.embed.fields ?? []) {
-        const name = replace(field.name)
-        const content = replace(field.content)
-
-        embed.addField(name, content, field.inline)
-      }
-
       try {
+        const embed = generateEmbed(hook, data)
         await webhook.send({ embeds: [embed] })
 
         ctx.status = 200
